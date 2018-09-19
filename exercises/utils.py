b36c8854854db4a977068f3d22975f598c71e741
Nate Browne <natebrowne@outlook.com>
2018-09-18 23:48:52 -0700

    Finished chapter 10, starting on clustering bonus material

--- exercises/utils.py
+++ exercises/utils.py
@@ -5,8 +5,13 @@ def find_and_remove(name, from_home=True):
     '''Helper function for deleting datasets. Some datasets are a bit finnicky
     about loading them in, so this function can be used to remove them before
     redownloading.'''
+    removed = False
     path = expanduser('~') if from_home else '/'
     for root, dirs, files in walk(path):
         if name in files:
             remove(join(root, name))
             rmdir(root)
+            removed = True
+
+    if not removed:
+        print("File not found")
